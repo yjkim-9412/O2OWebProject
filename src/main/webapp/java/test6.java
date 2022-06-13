77b4c94e6ba2ccf41a689f70417223ad7c4b453a
Lamia9304 <lamia9304@naver.com>
2022-06-13 20:52:40 +0900

    test

---
+++ src/main/webapp/java/test6.java
@@ -0,0 +1,34 @@
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+
+public class test1 {
+
+
+    interface GithubService {
+        GitHub connect() throws IOException;
+    }
+
+    class DefaultGitHubService implements GithubService {
+        @Override
+        public GitHub connect() {
+            return null;
+        }
+    }
+    private GithubService githubService;
+    public test1(GithubService githubService){
+        this.githubService = githubService;
+    }
+    public int getPoint(String repositoryname) throws IOException {
+        GitHub gitHub = githubService.connect();
+        return 0;
+    }
+
+    public void test(){
+       public void and(){
+        public and(){
+            
+            }
+        }
+    }
+}
