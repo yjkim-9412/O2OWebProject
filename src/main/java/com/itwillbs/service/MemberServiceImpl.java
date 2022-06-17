9a7e9eaf1634b0b1d149e770dded4cf91bfb9809
DESKTOP-FKD5EKF\ITWILL <a99010703@gmail.com>
2022-06-17 12:49:36 +0900

    Revert "kakao login"
    This reverts commit 151942f7d9a89e5838e4705bce6c911f830c8421.

--- src/main/java/com/itwillbs/service/MemberServiceImpl.java
+++ src/main/java/com/itwillbs/service/MemberServiceImpl.java
@@ -44,50 +44,6 @@ public class MemberServiceImpl implements MemberService{
 		
 	}
 	
-	//  ----------- 카카오 API ----------- 
-	public HashMap<String, Object> getUserInfo(String access_Token) {
-
-		// 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
-		HashMap<String, Object> userInfo = new HashMap<String, Object>();
-		String reqURL = "https://kapi.kakao.com/v2/user/me";
-		try {
-			URL url = new URL(reqURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-
-			// 요청에 필요한 Header에 포함될 내용
-			conn.setRequestProperty("Authorization", "Bearer " + access_Token);
-
-			int responseCode = conn.getResponseCode();
-			System.out.println("responseCode : " + responseCode);
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-			String line = "";
-			String result = "";
-
-			while ((line = br.readLine()) != null) {
-				result += line;
-			}
-			System.out.println("response body : " + result);
-
-			JsonParser parser = new JsonParser();
-			JsonElement element = parser.parse(result);
-
-			JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
-			JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-
-			String nickname = properties.getAsJsonObject().get("nickname").getAsString();
-			String email = kakao_account.getAsJsonObject().get("email").getAsString();
-
-			userInfo.put("nickname", nickname);
-			userInfo.put("email", email);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return userInfo;
-	}
 	
 	
 
