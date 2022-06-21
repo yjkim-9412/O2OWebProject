21e9d731228106fb6fef29a8acca71921f8bd795
JackGhL <xxt1205@gmail.com>
2022-06-21 15:27:47 +0900

    PROStable JDBC setting

---
+++ src/main/java/com/itwillbs/controller/ProController.java
@@ -0,0 +1,34 @@
+package com.itwillbs.controller;
+
+import com.itwillbs.domain.MemberDTO;
+import javax.servlet.http.HttpSession;
+
+import com.itwillbs.domain.ProDTO;
+import com.itwillbs.service.ProService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.inject.Inject;
+
+@Controller
+public class ProController {
+
+    @Inject
+        private ProService proService;
+
+    @RequestMapping(value = "/pro/proInsert", method = RequestMethod.GET)
+    public String proInsert() {
+
+        return "pro/ProInsertForm";
+    }
+
+    @RequestMapping(value = "/pro/InsertPro", method = RequestMethod.GET)
+    public String InsertPro(ProDTO proDTO) {
+        proService.insertPro(proDTO);
+        return "redirect:/pro/login";
+    }
+}
---
+++ src/main/java/com/itwillbs/service/ProServiceImpl.java
@@ -0,0 +1,12 @@
+package com.itwillbs.service;
+
+import com.itwillbs.domain.ProDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProServiceImpl implements ProService {
+    @Override
+    public void insertPro(ProDTO proDTO) {
+
+    }
+}
