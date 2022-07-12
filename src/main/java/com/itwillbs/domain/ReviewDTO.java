7ca0b482b53078b7a42ad77419664850a3a9b605
Heojihye <a2420764@gmail.com>
2022-07-12 10:20:16 +0900

    reviewDTO pageDTO pom.xml getter setter 수정

--- src/main/java/com/itwillbs/domain/ReviewDTO.java
+++ src/main/java/com/itwillbs/domain/ReviewDTO.java
@@ -1,12 +1,10 @@
 package com.itwillbs.domain;
 
-import lombok.Getter;
-import lombok.Setter;
 
-import java.sql.Timestamp;
 
-@Getter
-@Setter
+
+
+
 public class ReviewDTO {
 	private int id;
 	private int account_id;
@@ -15,5 +13,47 @@ public class ReviewDTO {
 	private String date;
 	private int rating;
 	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+	public int getPro_id() {
+		return pro_id;
+	}
+	public void setPro_id(int pro_id) {
+		this.pro_id = pro_id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
