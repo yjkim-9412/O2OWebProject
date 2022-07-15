b9bb052df9d864182985fd92ef6f24b83c96bd61
hyun <gsutmd262@naver.com>
2022-07-15 16:31:40 +0900

    no message

--- src/main/java/com/itwillbs/controller/MemberController.java
+++ src/main/java/com/itwillbs/controller/MemberController.java
@@ -8,13 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.itwillbs.domain.MemberDTO;
 import com.itwillbs.domain.*;
 import com.itwillbs.service.MemberService;
 
@@ -240,6 +241,22 @@ System.out.println("#########" + code);
 		}
 	}
 
+	@RequestMapping(value = "/member/dupcheck", method = RequestMethod.GET)
+	public ResponseEntity<String> dupcheck(HttpServletRequest request) {
+		ResponseEntity<String> entitiy=null;
+		String result="";
+		
+		String email=request.getParameter("email");
+		MemberDTO memberDTO= memberService.getMemberE(email);
+		
+		if(memberDTO!=null) {
+			result="emaildup";
+		}else {
+			result="emailok";
+		}
 
+		entitiy=new ResponseEntity<String>(result,HttpStatus.OK);
+		return entitiy;
+	}
 }
 
